f894c794227e47419b3a8ef8ea897921ac537ceb
ymarquina <yeseniamrqna@gmail.com>
2023-03-26 12:09:00 -0700

    fix api url

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -3,7 +3,7 @@
 // if the city name is valid error is thrown and displayed
 async function getCoordinates(cityName){
   try{
-    let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},{us}&appid=84be7224f83f2b9047883638524cc4d6`,
+    let response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName},{us}&appid=84be7224f83f2b9047883638524cc4d6`,
     {mode: 'cors'});
     let coordinates = await response.json();
     return [coordinates[0].lat, coordinates[0].lon]
